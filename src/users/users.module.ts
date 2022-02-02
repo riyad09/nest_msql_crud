86c6c29b6706d133b8337fe8bac7b56b627aa253
rashadulalam <riyad2006@gmail.com>
2022-02-02 23:01:49 +0600

    controller created

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersEntity } from './users.entity';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsersEntity])],
